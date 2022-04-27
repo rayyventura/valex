85ab0a770751d0a06d0be9bbfd92f668d4286643
iagod <iago.tostes@cubos.io>
2022-04-27 17:22:27 -0300

    Preparando revisão de código

--- src/schemas/paymentSchema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-const paymentSchema = Joi.object({
-  cardId: Joi.number().integer().required(),
-  password: Joi.string().max(4).min(4).required(),
-  businessId: Joi.number().integer().required(),
-  amount: Joi.number().min(1).integer().required(),
-});
-
-export default paymentSchema;
--- src/schemas/rechargeSchema.ts
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-
-const rechargeSchema = Joi.object({
-  amount: Joi.number().integer().required(),
-});
-
-export default rechargeSchema;
